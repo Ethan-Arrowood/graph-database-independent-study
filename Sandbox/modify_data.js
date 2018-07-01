b2375fb3d3005d2c3c7f280e8156140ea63b1a65
Ethan Arrowood <ethan.arrowood@gmail.com>
2018-07-01 12:07:40 -0400

    renames

--- Sandbox/modify_data.js
+++ Sandbox/modify_data.js
@@ -1,5 +1,5 @@
-const fs = require('fs')
-const data = require('./data.json').testsByDept;
+const fs = require("fs");
+const data = require("./data.json").testsByDept;
 
 const new_data = Object.keys(data).reduce((acc, dept) => {
   return acc.concat({
@@ -8,13 +8,13 @@ const new_data = Object.keys(data).reduce((acc, dept) => {
       return acc.concat({
         rank: rank.toLowerCase(),
         tests: Object.values(data[dept][rank]).reduce((acc, test) => {
-          return acc.concat(test.toLowerCase())
+          return acc.concat(test.toLowerCase());
         }, [])
-      })
+      });
     }, [])
-  })
+  });
 }, []);
 
 console.log(new_data);
 
-fs.writeFileSync('query_data.json', JSON.stringify(new_data));
+fs.writeFileSync("query_data.json", JSON.stringify(new_data));
